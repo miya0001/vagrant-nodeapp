d17ff138accbcd679ffca7f7408e5e180770564c
miya0001 <miya@wpist.me>
2016-03-29 05:28:39 +0900

    yes\!

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo npm cache clean
 sudo npm install n -g
 sudo n latest
 sudo ln -sf /usr/local/bin/node /usr/bin/node
-sudo apt-get remove nodejs npm
+sudo apt-get remove nodejs npm -y
 sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
 
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
