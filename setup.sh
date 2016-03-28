8bd8daabc3b40077db9fef4e57334ec26f1d93ac
miya0001 <miya@wpist.me>
2016-03-29 07:54:21 +0900

    add host key of github.com automatically

--- setup.sh
+++ setup.sh
@@ -8,7 +8,9 @@ sudo ln -sf /usr/local/bin/node /usr/bin/node
 sudo apt-get remove nodejs npm -y
 sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
 
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+if [[ ! -d /root/.ssh ]]; then
+  ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+fi
 
 if [ -n "$GIT_REPO" ]; then
   git clone $GIT_REPO /vagrant/www
